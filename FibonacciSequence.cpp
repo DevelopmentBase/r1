c4b6c10fc24f9ef3b649320ed7702629d537b4e8
Hamed Azaddoulat <82142255+DevelopmentBase@users.noreply.github.com>
2023-04-15 18:41:49 +0300

    Fibonacci Sequence
    The program prints Fibonacci Sequence less than 255 to the console.

---
+++ FibonacciSequence.cpp
@@ -0,0 +1,28 @@
+#using <mscorlib.dll>
+#using <System.dll>
+
+using namespace System;
+
+/*************************************************
+* Description:
+* Finbonacci series starts from 0, then 1, then
+* the next digit is evaluated from the sum of
+* previous two numbers, and so on.
+*
+* 0, 1, 1(0+1), 2(1+1), 3(2+1), 5(3+2), 8(5+3)...
+*
+* The below program prints Fibonacci series less
+* than number 255 to the console.
+*************************************************/
+
+int main(array<String ^> ^args)
+{
+    int pre(0), cur(1), nxt(0);
+    do {
+        Console::WriteLine(pre);
+        nxt = pre + cur;
+        pre = cur;
+        cur = nxt;
+    } while (pre < 255);
+    return 0;
+}

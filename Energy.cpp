d6ef70d642bb009bc650ef87e3fed63cf85a6e63
Hamed Azaddoulat <82142255+DevelopmentBase@users.noreply.github.com>
2023-04-23 22:48:44 +0300

    Update Energy.cpp

--- Energy.cpp
+++ Energy.cpp
@@ -3,6 +3,15 @@
 
 using namespace System;
 
+/*************************************************
+* Description:
+* The following formula represents Einstein's
+* famous energy equation: E = mc^2, where
+* E = Energy,
+* m = mass,
+* c = speed of light.
+*************************************************/
+
 int main(array<String ^> ^args)
 {
     return 0;

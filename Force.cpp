abc72d502ce4e7a0049d629d93b7e24b5f8a0719
Hamed Azaddoulat <82142255+DevelopmentBase@users.noreply.github.com>
2023-04-24 23:04:37 +0300

    Update Force.cpp

--- Force.cpp
+++ Force.cpp
@@ -3,6 +3,16 @@
 
 using namespace System;
 
+/*************************************************
+* Description:
+* The following formula represents Newton's
+* second law of motion, force equation: F = ma,
+* where
+* F = Force,
+* m = mass,
+* a = acceleration.
+*************************************************/
+
 int main(array<String ^> ^args)
 {
     return 0;

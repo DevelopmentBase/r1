969cf9fae81fd611afe98f4f6b4f2411b2939fc2
Hamed Azaddoulat <82142255+DevelopmentBase@users.noreply.github.com>
2023-04-25 21:56:35 +0300

    Update OhmLaw.cpp

--- OhmLaw.cpp
+++ OhmLaw.cpp
@@ -3,6 +3,15 @@
 
 using namespace System;
 
+/*************************************************
+* Description:
+* The following formula represents Ohm's law:
+* R = V/I, where
+* R = resistance,
+* V = potential (potential difference) (volt),
+* I = current intensity (ampere).
+*************************************************/
+
 int main(array<String ^> ^args)
 {
     return 0;
